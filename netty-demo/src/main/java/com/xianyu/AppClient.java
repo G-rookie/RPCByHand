e3ed6561c0b96f1599f81ca180d659cdf5f5d6ea
xianyu-3 <1539468306@qq.com>
2023-08-08 21:57:52 +0800

    了解netty的工作流程并编写一个小demo，创建一个客户端

---
+++ netty-demo/src/main/java/com/xianyu/AppClient.java
@@ -0,0 +1,49 @@
+package com.xianyu;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+import java.net.InetSocketAddress;
+import java.nio.charset.StandardCharsets;
+
+public class AppClient {
+    private void run() {
+        //定义线程池，EventLoopGroup
+        NioEventLoopGroup group = new NioEventLoopGroup();
+
+        //启动一个客户端需要一个辅助类，bootStrap
+        Bootstrap bootstrap = new Bootstrap();
+        bootstrap = bootstrap.group(group)
+                .remoteAddress(new InetSocketAddress(8080))
+                //选择初始化一个什么洋的channel
+                .channel(NioSocketChannel.class)
+                .handler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    protected void initChannel(SocketChannel socketChannel) {
+
+                    }
+                });
+
+        //尝试连接服务器
+        ChannelFuture channelFuture;
+        try {
+            channelFuture = bootstrap.connect().sync();
+            //获取channel，并且写入数据
+            channelFuture.channel().writeAndFlush(Unpooled.copiedBuffer("hello netty!".getBytes(StandardCharsets.UTF_8)));
+
+            //阻塞程序，等待接受消息
+            channelFuture.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        new AppClient().run();
+    }
+}
