bfe6b182bf51402c4a29cecf4fff3a9265fa1ad0
xianyu-3 <1539468306@qq.com>
2023-08-15 22:31:32 +0800

    了解ZooKeeper的概念，写一个小demo了解最基本的用法

---
+++ netty-demo/src/test/java/com/xianyu/ZookeeperTest.java
@@ -0,0 +1,87 @@
+package com.xianyu;
+
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.ZooDefs;
+import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.data.Stat;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+public class ZookeeperTest {
+    ZooKeeper zooKeeper;
+
+    @Before
+    public void createZK() {
+
+        //定义一个连接参数
+        String connectString = "127.0.0.1:2181";
+
+        //定义超时时间
+        int timeout = 1000;
+
+        try {
+            zooKeeper = new ZooKeeper(connectString, timeout, null);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Test
+    public void testCreatePNode() {
+        String result;
+        try {
+            result = zooKeeper.create("/xianyu", "hello".getBytes(StandardCharsets.UTF_8),
+                    ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+            System.out.println("result = " + result);
+        } catch (KeeperException | InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (zooKeeper == null) {
+                    assert false;
+                    zooKeeper.close();
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Test
+    public void testExistPNode() {
+        try {
+            // version: cas 乐观锁 ， 也可以无视版本号 -1
+            Stat stat = zooKeeper.exists("/xianyu", null);
+
+            //当前结点的数据版本
+            zooKeeper.setData("/xianyu", "world".getBytes(StandardCharsets.UTF_8), -1);
+
+            int version = stat.getVersion();
+            System.out.println("version = " + version);
+
+            //当前结点的acl数据版本
+            int aversion = stat.getAversion();
+            System.out.println("aversion = " + aversion);
+
+            //当前子节点数据的版本
+            int cversion = stat.getCversion();
+            System.out.println("cversion = " + cversion);
+        } catch (KeeperException | InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (zooKeeper == null) {
+                    assert false;
+                    zooKeeper.close();
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
